1ca5e1d14942b61b3bae83073370ab662e43bc94
wangcy916 <wangcy916@163.com>
2019-07-13 16:54:06 +0800

    add code

--- wchy-srv/src/main/java/com/wchy/demo/srv/service/AccountService.java
+++ wchy-srv/src/main/java/com/wchy/demo/srv/service/AccountService.java
@@ -1,4 +1,11 @@
 package com.wchy.demo.srv.service;
 
+import com.wchy.demo.basedao.dto.Account;
+
 public interface AccountService {
+
+    String SERVICE_ID = "accountService";
+
+    Account getAccount();
+
 }
--- wchy-srv/src/main/java/com/wchy/demo/srv/service/impl/AccountServiceImpl.java
+++ wchy-srv/src/main/java/com/wchy/demo/srv/service/impl/AccountServiceImpl.java
@@ -1,6 +1,19 @@
 package com.wchy.demo.srv.service.impl;
 
+import com.wchy.demo.basedao.dao.AccountDao;
+import com.wchy.demo.basedao.dto.Account;
 import com.wchy.demo.srv.service.AccountService;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
+@Service(AccountService.SERVICE_ID)
 public class AccountServiceImpl implements AccountService {
+
+    @Resource
+    private AccountDao accountDao;
+
+    public Account getAccount() {
+        return accountDao.getAccount();
+    }
 }
